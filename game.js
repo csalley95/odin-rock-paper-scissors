b4ebff4e0c2930f9f2bc825d54e713a80666c0df
Chris Salley <csalley1995@gmail.com>
2026-09-13 17:19:12 -0400

    Moved my playRound function and score variables so that they’re declared inside of the new playGame function.

--- game.js
+++ game.js
@@ -15,35 +15,33 @@ function getHumanChoice() {
     return choice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        return "It's a tie!";
-    }
-    if  ((humanChoice === "rock" && computerChoice === "scissors") ||
-         (humanChoice === "paper" && computerChoice === "rock") ||
-         (humanChoice === "scissors" && computerChoice === "paper")) {
-        humanScore++;
-        return "You win!";
-    } else {
-        computerScore++;
-        return "Computer wins!";
-    }
-}
-
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
 
 function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
     for (let i = 0; i < 5; i++) {
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
         const result = playRound(humanSelection, computerSelection);
         console.log(`Round ${i + 1}: You chose ${humanSelection}, Computer chose ${computerSelection}. ${result}`);
+    }
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            return "It's a tie!";
+        }
+        if  ((humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")) {
+            humanScore++;
+            return "You win!";
+        } else {
+            computerScore++;
+            return "Computer wins!";
+        }
     }
 }
 
